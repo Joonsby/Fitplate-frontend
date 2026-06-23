1e86bdb62252c223e16bd4c6a65104afafe464c7
azsoft <flamewizkey@naver.com>
2026-06-23 09:38:38 +0900

    fix : 로고 이미지, displayName, icon 수정

--- granite.config.ts
+++ granite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "fitplate",
   brand: {
-    displayName: "Fitplate",
+    displayName: "핏플레이트",
     primaryColor: "#3182F6", // TDS Button color="primary"에 적용되는 앱 기본 브랜드 색상입니다.
-    icon: "",
+    icon: "https://static.toss.im/appsintoss/43801/51460797-5c1e-431f-b0e9-14407cfbca5f.png",
   },
   web: {
     host: "localhost",
